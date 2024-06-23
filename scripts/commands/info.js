6c8f24a6cceb5ad18103c49928d76bcb3fa55ffd
Farabi01744 <mejajhot280@gmail.com>
2024-06-23 19:50:22 +0300

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,20 +28,20 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : ❤️‍🔥💥ফারাবী✨ইসলাম✨রনি💥❤️‍🔥
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : FARABI ISLAM RONY 
+𝐍𝐚𝐦𝐞       : ❤️‍🔥💥md ✨Mominur✨Islam💥❤️‍🔥
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : মনের রানি তুমি
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : ইসলাম
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: টাঙ্গাইল, ঢাকা
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: বলবো না, কালিহাতী, টাঙ্গাইল, ঢাকা, 
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: সিরাজগঞ্জ, 
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: সিরাজগঞ্জ, 
 𝐆𝐞𝐧𝐝𝐞𝐫.   : ছেলে
-𝐀𝐠𝐞           : ২৩+
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : সিঙ্গেল
+𝐀𝐠𝐞           : ২০+
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : বলা যাবে না
 𝐖𝐨𝐫𝐤        : JOB (কাজ)
-𝐆𝐦𝐚𝐢𝐥       : farabii708@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+966531079542
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/farabi0173210
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/farabi0173210`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐆𝐦𝐚𝐢𝐥       : mb411820@gmail
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801709384267
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/unknown
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=61550551112292`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://i.imgur.com/3TrFdZC.jpeg`)).pipe(
+      return request(encodeURI(`"https://i.imgur.com/6REK8Zs.jpeg",`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
